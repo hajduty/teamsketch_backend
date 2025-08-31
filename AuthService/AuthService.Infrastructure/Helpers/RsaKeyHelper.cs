eea9ad712735f41aee224d13baa66a445aaaae67
hajduty <hajduty@outlook.com>
2025-08-31 11:45:39 +0200

    use rsa instead of hmac for jwt

---
+++ AuthService/AuthService.Infrastructure/Helpers/RsaKeyHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography;
+
+namespace AuthService.Infrastructure.Helpers
+{
+    public static class RsaKeyHelper
+    {
+        public static RSA LoadOrCreate(IConfiguration config)
+        {
+            var rsa = RSA.Create(2048);
+            var keypath = config["Jwt:KeyPath"];
+
+            if (File.Exists(keypath))
+            {
+                var keyText = File.ReadAllText(keypath);
+                rsa.FromXmlString(keyText);
+            }
+            else
+            {
+                var keyText = rsa.ToXmlString(true);
+                File.WriteAllText(keypath!, keyText);
+            }
+
+            return rsa;
+        }
+    }
+}
