c4843fc1fe853e03a464cd2d6a004c0fd0e5e55a
hajduty <hajduty@outlook.com>
2025-09-03 12:37:58 +0200

    add db connectionstring & register db service

--- PermissionService/PermissionService.Infrastructure/DependencyInjection.cs
+++ PermissionService/PermissionService.Infrastructure/DependencyInjection.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PermissionService.Core.Interfaces;
+using PermissionService.Infrastructure.Data;
 using PermissionService.Infrastructure.Repositories;
 
-namespace PermissionService.Infrastructure
-{
-    public static class DependencyInjection
-    {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddScoped<IPermissionRepository, PermissionRepository>();
-            services.AddScoped<IPermissionService, Services.PermissionService>();
+namespace PermissionService.Infrastructure;
 
-            return services;
-        }
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IPermissionRepository, PermissionRepository>();
+        services.AddScoped<IPermissionService, Services.PermissionService>();
+
+        return services;
     }
 }
