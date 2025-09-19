5f0247b68faf7236fd265e1a944ed4eef0cbd73b
hajduty <hajduty@outlook.com>
2025-09-19 11:46:30 +0200

    send jwt token instead of useremail in grpc call

--- PermissionService/PermissionService.API/Services/PermissionGrpcService.cs
+++ PermissionService/PermissionService.API/Services/PermissionGrpcService.cs
@@ -1,6 +1,8 @@
 ﻿using Grpc.Core;
 using Microsoft.AspNetCore.Identity;
 using PermissionService.Core.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
 using UserService.Grpc;
 using static PermissionService.API.Permission;
 
@@ -10,11 +12,22 @@ namespace PermissionService.API.Services
     {
         public override async Task<PermissionResponse> CheckPermission(PermissionRequest request, ServerCallContext context)
         {
-            var permission = await permissionService.GetUserPermission(request.UserEmail, request.Room);
+            var userEmail = "";
+            Console.WriteLine("HELLOOOOOOO FROM PERMISSIONSERVICE");
+            try
+            {
+                var handler = new JwtSecurityTokenHandler();
+                var token = handler.ReadJwtToken(request.Token);
+                var emailClaim = token.Claims.FirstOrDefault(c => c.Type == "email");
+                userEmail = emailClaim?.Value ?? throw new RpcException(new Status(StatusCode.Unauthenticated, "Email claim not found in token."));
+            }
+            catch { throw new RpcException(new Status(StatusCode.Unauthenticated, "Invalid token.")); }
+
+            var permission = await permissionService.GetUserPermission(userEmail, request.Room);
 
             if (permission == null)
             {
-                return new PermissionResponse { Role = "None", UserEmail = permission.UserEmail, Room = permission.Room };
+                return new PermissionResponse { Role = "None", UserEmail = userEmail, Room = request.Room };
             }
 
             return new PermissionResponse { Role = permission.Role, UserEmail = permission.UserEmail, Room = permission.Room };
