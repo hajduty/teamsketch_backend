6f27341cf491ad1e80af52f04828623b69e4c6d7
Hajder Al-Remahy <60933760+hajduty@users.noreply.github.com>
2025-09-24 08:55:08 +0200

    feat/permission-datetime (#11)
    * add timestamp to permission

--- PermissionService/PermissionService.Infrastructure/Migrations/20250924062948_AddCreatedAt.Designer.cs
+++ PermissionService/PermissionService.Infrastructure/Migrations/20250924062948_AddCreatedAt.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PermissionService.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using PermissionService.Infrastructure.Data;
 namespace PermissionService.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250924062948_AddCreatedAt")]
+    partial class AddCreatedAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
